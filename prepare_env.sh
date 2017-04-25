bfa45213a641e3d36a516d3f631462bce5ef9145
David Gonzalez <dgonzalez@bitnami.com>
2017-04-25 04:18:11 +0000

    First :boom:

---
+++ prepare_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+## Mirrors for CLANG 3.9 enforcement are added
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+echo "yes" | sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
+
+sudo apt-get -qq update
+
+sudo apt-get -qq install clang-3.9
+
+sudo apt-get -qq install build-essential libtool make cmake cmake-data openssl
+sudo apt-get -qq install libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev
+sudo apt-get -qq install libboost1.55-dev libboost-thread1.55-dev libboost-filesystem1.55-dev libboost-system1.55-dev libboost-program-options1.55-dev libboost-iostreams1.55-dev
+
+export CC=clang-3.9 CXX=clang++-3.9
+
+## The repository with the source to compile is cloned 
+git clone -b 3.3.5 --single-branch https://github.com/TrinityCore/TrinityCore 
---
+++ publish_3.3.5.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+tcd() {
+  cd $TRAVIS_BUILD_DIR/$1
+}
+
+echo $((`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION` + 1)) > $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION
+export GIT_TAG=`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_VERSION`-r`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION`
+
+tar vczf trinitycore-$GIT_TAG.tar.gz trinitycore
+mv trinitycore-*.tar.gz $TRAVIS_BUILD_DIR
+
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+git fetch --tags
+
+if [[ git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER" 2>/dev/null ]] ; then 
+  git add TRINITYCORE_NAMI_REVISION
+  git commit -m "[TravisCI] Increase Revision file [ci skip]" 
+  git push -q https://$GH_TOKEN@github.com/dgonzalezruiz/TrinityCore.git --follow-tags 
+else 
+  echo "Tag already exists!"
+fi
+
