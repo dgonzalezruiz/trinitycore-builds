4f7ec1f69fc0ed30f35cbfdebcc52eb2f292b3ed
David Gonzalez <dgonzalez@bitnami.com>
2017-04-25 04:56:55 +0000

    General improvements to publishing shell script

--- publish_3.3.5.sh
+++ publish_3.3.5.sh
@@ -3,26 +3,28 @@
 set -e
 
 tcd() {
-  cd $TRAVIS_BUILD_DIR/$1
+  cd "$TRAVIS_BUILD_DIR"/"$1"
 }
 
-echo $((`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION` + 1)) > $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION
+NEW_TRINITYCORE_REVISION=$((`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION` + 1))
+echo "$NEW_TRINITYCORE_REVISION" > "$TRAVIS_BUILD_DIR"/TRINITYCORE_NAMI_REVISION
+
 export GIT_TAG=`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_VERSION`-r`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION`
 
-tcd TrinityCore/bin
-tar vczf trinitycore-$GIT_TAG.tar.gz trinitycore
-mv trinitycore-*.tar.gz $TRAVIS_BUILD_DIR
+tcd "TrinityCore/bin"
+tar vczf trinitycore-"$GIT_TAG".tar.gz trinitycore
+mv trinitycore-*.tar.gz "$TRAVIS_BUILD_DIR"
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git fetch --tags
 
-cd $TRAVIS_BUILD_DIR
+cd "$TRAVIS_BUILD_DIR"
 
-if [[ git tag $GIT_TAG -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER" 2>/dev/null ]] ; then 
+if git tag "$GIT_TAG" -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER" 2>/dev/null ; then 
   git add TRINITYCORE_NAMI_REVISION
   git commit -m "[TravisCI] Increase Revision file [ci skip]" 
-  git push -q https://$GH_TOKEN@github.com/dgonzalezruiz/TrinityCore.git --follow-tags 
+  git push -q https://"$GH_TOKEN"@github.com/dgonzalezruiz/TrinityCore.git --follow-tags 
 else 
   echo "Tag already exists!"
 fi
