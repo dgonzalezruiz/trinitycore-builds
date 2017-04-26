f13b4d4f6557032a842017a0c813f4d13ed0a266
David González <dgonzalez@bitnami.com>
2017-04-27 00:57:59 +0200

    Fix facepalm

--- build_3.3.5.sh
+++ build_3.3.5.sh
@@ -20,7 +20,7 @@ if [ ! -f $TRAVIS_BUILD_DIR/trinitycore/bin/worldserver ] ; then
 
   log "=============================="
   log "Configuring the compilation..."
-  cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSERVERS=1 -DNOJEM=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BRANCH/trinitycore
+  cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSERVERS=1 -DNOJEM=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BUILD_DIR/trinitycore
   log "=============================="
 
   tcd TrinityCore && chmod +x contrib/check_updates.sh
