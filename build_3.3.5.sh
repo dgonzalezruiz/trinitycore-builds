b6c614dd72f0612e0523f703e2060ef70071090a
David González <dgonzalez@bitnami.com>
2017-04-27 00:05:28 +0200

    [ci skip] Change compilation prefix

--- build_3.3.5.sh
+++ build_3.3.5.sh
@@ -20,7 +20,7 @@ if [ ! -f $TRAVIS_BUILD_DIR/Trinitycore/bin/trinitycore/bin/worldserver ] ; then
 
   log "=============================="
   log "Configuring the compilation..."
-  cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSERVERS=1 -DNOJEM=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_INSTALL_PREFIX=trinitycore
+  cmake ../ -DWITH_WARNINGS=1 -DWITH_COREDEBUG=0 -DUSE_COREPCH=1 -DUSE_SCRIPTPCH=1 -DTOOLS=1 -DSERVERS=1 -DNOJEM=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-Werror" -DCMAKE_CXX_FLAGS="-Werror" -DCMAKE_INSTALL_PREFIX=$TRAVIS_BRANCH/trinitycore
   log "=============================="
 
   tcd TrinityCore && chmod +x contrib/check_updates.sh
@@ -49,8 +49,6 @@ else
   log "======================================================"
 fi
 
-tcd TrinityCore/bin/trinitycore/bin
+tcd trinitycore/bin
 ./worldserver --version
 ./authserver --version
-
-tcd TrinityCore/bin
