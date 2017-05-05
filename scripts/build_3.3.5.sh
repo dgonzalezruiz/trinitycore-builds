6722513eea9462f52750165a099c73f41c1a6cc1
David Gonzalez <dgonzalez@bitnami.com>
2017-05-05 19:20:22 +0000

    Fix typo in build process

--- scripts/build_3.3.5.sh
+++ scripts/build_3.3.5.sh
@@ -14,8 +14,8 @@ if [ ! -f $TRAVIS_BUILD_DIR/trinitycore/bin/worldserver ] ; then
   ## This folder is created in case it does not exist before. The compilation will be released here
   tcd 
   mkdir -p $TRAVIS_BUILD_DIR/trinitycore
-  docker build --tag buildEnvironment . 
-  docker run -it -v $TRAVIS_BUILD_DIR/TrinityCore:/TrinityCore -v $TRAVIS_BUILD_DIR/trinitycore:/trinitycore buildEnvironment 
+  docker build --tag build_environment . 
+  docker run -it -v $TRAVIS_BUILD_DIR/TrinityCore:/TrinityCore -v $TRAVIS_BUILD_DIR/trinitycore:/trinitycore build_environment
 else 
   log "======================================================"
   log "Skipping compilation as a cached compilation was found"
