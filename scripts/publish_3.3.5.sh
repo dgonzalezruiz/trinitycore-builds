20dea9f542e9d17c6e481280bd7033315d4039bc
David Gonzalez <dgonzalez@bitnami.com>
2017-05-05 18:56:55 +0000

    Simplify tagging system

--- scripts/publish_3.3.5.sh
+++ scripts/publish_3.3.5.sh
@@ -6,26 +6,26 @@ tcd() {
   cd "$TRAVIS_BUILD_DIR"/"$1"
 }
 
-NEW_TRINITYCORE_REVISION=$((`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION` + 1))
-echo "$NEW_TRINITYCORE_REVISION" > "$TRAVIS_BUILD_DIR"/TRINITYCORE_NAMI_REVISION
+git fetch --tags
+GIT_TAG="$(git tag | tail -n1)"
 
-export GIT_TAG=`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_VERSION`-r`cat $TRAVIS_BUILD_DIR/TRINITYCORE_NAMI_REVISION`
+
+TRINITYCORE_REVISION=$(($(echo $GIT_TAG | cut -d'r' -f2) + 1))
+TRINITYCORE_VERSION=$(echo $GIT_TAG | cut -d'-' -f1)
+
+export GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_REVISION
 
 tcd
 tar vczf trinitycore-"$GIT_TAG".tar.gz trinitycore
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-git fetch --tags
 
 cd "$TRAVIS_BUILD_DIR"
 
 git checkout $TRAVIS_BRANCH
 
 if git tag "$GIT_TAG" -a -m "Checking if the tag already exists..." 2>/dev/null ; then
-  git add TRINITYCORE_NAMI_REVISION
-  git commit -m "[TravisCI] Increase Revision file [ci skip]"
-  git push -q https://"$GH_TOKEN"@github.com/dgonzalezruiz/trinitycore-builds.git 1> /dev/null
   ## The tag is deleted (locally), so that the latest commit for the revision file is included in the tag
   ## When retagging now.
   git tag -d "$GIT_TAG"
