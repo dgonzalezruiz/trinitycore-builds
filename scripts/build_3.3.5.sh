46aa97baa67431aa61c167665a3cda11c28203cf
David González <dgonzalez@bitnami.com>
2017-05-07 15:04:07 +0200

    Fix testing of binaries compiled

--- scripts/build_3.3.5.sh
+++ scripts/build_3.3.5.sh
@@ -16,12 +16,10 @@ if [ ! -f $TRAVIS_BUILD_DIR/trinitycore/bin/worldserver ] ; then
   mkdir -p $TRAVIS_BUILD_DIR/trinitycore
   docker build --tag build_environment . 
   docker run -it -v $TRAVIS_BUILD_DIR/TrinityCore:/TrinityCore -v $TRAVIS_BUILD_DIR/trinitycore:/trinitycore build_environment
+  docker run -it -v $TRAVIS_BUILD_DIR/TrinityCore:/TrinityCore -v $TRAVIS_BUILD_DIR/trinitycore:/trinitycore build_environment /trinitycore/bin/authserver --version
+  docker run -it -v $TRAVIS_BUILD_DIR/TrinityCore:/TrinityCore -v $TRAVIS_BUILD_DIR/trinitycore:/trinitycore build_environment /trinitycore/bin/worldserver --version
 else 
   log "======================================================"
   log "Skipping compilation as a cached compilation was found"
   log "======================================================"
 fi
-
-tcd trinitycore/bin
-./worldserver --version
-./authserver --version
