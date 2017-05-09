8665d13f6e513f3baf13d3c4bcca7d2f456877cc
David González <dgonzalez@bitnami.com>
2017-05-09 05:39:09 +0200

    Fix tarball tagging behaviour

--- scripts/publish_3.3.5.sh
+++ scripts/publish_3.3.5.sh
@@ -7,21 +7,20 @@ tcd() {
 }
 
 git fetch --tags
-GIT_TAG="$(git tag | tail -n1)"
+TARBALL_TAG="$(git tag | tail -n1)"
 
-TRINITYCORE_REVISION_OLD=$(echo $GIT_TAG | cut -d'r' -f2)
-TRINITYCORE_REVISION_NEW=$(($(echo $GIT_TAG | cut -d'r' -f2) + 1))
-TRINITYCORE_VERSION=$(echo $GIT_TAG | cut -d'-' -f1)
 
-export GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_REVISION_NEW
+TRINITYCORE_REVISION_OLD=$(echo $TARBALL_TAG | cut -d'r' -f2)
+TRINITYCORE_REVISION_NEW=$(($(echo $TARBALL_TAG | cut -d'r' -f2) + 1))
+TRINITYCORE_VERSION=$(echo $TARBALL_TAG | cut -d'-' -f1)
+
+GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_REVISION_NEW
 
 tcd
-tar vczf trinitycore-"$GIT_TAG".tar.gz trinitycore
+tar vczf trinitycore-"$TARBALL_TAG".tar.gz trinitycore
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
-
-cd "$TRAVIS_BUILD_DIR"
 
 git checkout $TRAVIS_BRANCH
 
@@ -30,4 +29,6 @@ if [ -z $TRAVIS_TAG ] ; then
   git tag "$GIT_TAG" -a -m "Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER ; Changes: 
   $TRAVIS_COMMIT_MESSAGE" 1>/dev/null
   git push -q https://"$GH_TOKEN"@github.com/dgonzalezruiz/trinitycore-builds.git --tags 2>&1 1>/dev/null
+else
+  
 fi
