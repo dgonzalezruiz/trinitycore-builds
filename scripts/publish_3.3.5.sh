bf3ce4b709afeb73391aff1e7003f7e568d2bf27
David Gonzalez <dgonzalez@bitnami.com>
2017-05-13 21:52:04 +0000

    Refactor publishing script and fix bug in tarballs released

--- scripts/publish_3.3.5.sh
+++ scripts/publish_3.3.5.sh
@@ -6,23 +6,27 @@ tcd() {
   cd "$TRAVIS_BUILD_DIR"/"$1"
 }
 
-git fetch --tags
-TARBALL_TAG="$(git tag | tail -n1)"
-
-
-TRINITYCORE_REVISION_OLD=$(echo $TARBALL_TAG | cut -d'r' -f2)
-TRINITYCORE_REVISION_NEW=$(($(echo $TARBALL_TAG | cut -d'r' -f2) + 1))
-TRINITYCORE_VERSION=$(echo $TARBALL_TAG | cut -d'-' -f1)
-
-GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_REVISION_NEW
-
 tcd
-tar vczf trinitycore-"$TARBALL_TAG".tar.gz trinitycore
 
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
+git fetch --tags
 git checkout $TRAVIS_BRANCH
+
+CURRENT_TAG="$(git tag | tail -n1)"
+TRINITYCORE_NEW_REVISION=$(($(echo $CURRENT_TAG | cut -d'r' -f2) + 1))
+TRINITYCORE_VERSION=$(echo $CURRENT_TAG | cut -d'-' -f1)
+
+GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_NEW_REVISIONM
+
+if [ -z $TRAVIS_TAG ] ; then
+  # If this is a tagged build, then the tarball tag is that tag
+  # If it is not, them we tag the tarball the same way we will tag the repo
+  TARBALL_TAG=$CURRENT_TAG
+else TARBALL_TAG=$GIT_TAG
+
+tar czf trinitycore-"$TARBALL_TAG".tar.gz trinitycore
 
 if [ -z $TRAVIS_TAG ] ; then
   # Only tag if this is an untagged commit 
