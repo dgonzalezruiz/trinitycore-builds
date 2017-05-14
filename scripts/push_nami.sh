c2910fa4054eeed32b941d7acead9bb3686f092f
David Gonzalez <dgonzalez@bitnami.com>
2017-05-14 09:55:20 +0000

    Fix structure of pipelining script

--- scripts/push_nami.sh
+++ scripts/push_nami.sh
@@ -12,19 +12,17 @@ git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 git fetch --tags
 
+git clone https://github.com/dgonzalezruiz/nami-trinitycore
+tcd nami-trinitycore
+git fetch --tags
+git checkout $TRAVIS_BRANCH
+
 CURRENT_TAG="$(git tag | tail -n1)"
 
 TRINITYCORE_VERSION=$(echo $CURRENT_TAG | cut -d'-' -f1)
 TRINITYCORE_NEW_REVISION=$(($(echo $CURRENT_TAG | cut -d'r' -f2) + 1))
 
 GIT_TAG=$TRINITYCORE_VERSION-r$TRINITYCORE_NEW_REVISION
-
-git clone https://github.com/dgonzalezruiz/nami-trinitycore
-
-tcd nami-trinitycore
-
-git fetch --tags
-git checkout $TRAVIS_BRANCH
 
 # The above pushes a tag to the following repo in the pipeline, triggering a build consequently.
 
